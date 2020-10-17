b3c634bf5a7435e2dc1b62a587a3788f7df193f6
mngrm3a <3806081-mngrm3a@users.noreply.gitlab.com>
2020-10-17 22:55:34 +0200

    WIP: add haskell extension set

---
+++ pkgs/vscode-utils/extensionSets/haskell.nix
@@ -0,0 +1,12 @@
+{
+  settings = {
+    "[haskell]" = {
+      "editor.tabSize" = 2;
+    };
+    "files.exclude" = {
+      "{app;src;test}/**/*.{o;hi}" = true;
+      "dist-newstyle" = true;
+    };
+  };
+  extensions = [];
+}
