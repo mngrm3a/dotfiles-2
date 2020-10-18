50a7740ba698c105473a35bc4e71e7978b20b92b
mngrm3a <3806081-mngrm3a@users.noreply.gitlab.com>
2020-10-18 07:25:29 +0200

    Add development shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}, dotnix ? import ./. { inherit pkgs; } }:
+
+let
+  vscode = with (dotnix.vscode-utils);
+    vscodeEnvFromExtensionSets
+      ./.vscode-extensions.nix
+      (with extensionSet; [ common nix ]);
+in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs;[ rnix-lsp nixpkgs-fmt ];
+  nativeBuildInputs = with pkgs;[ vscode rnix-lsp nixpkgs-fmt ];
   buildInputs = [];
 }
